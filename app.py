282b8be2e7db418c2ccf0eb7a0f981429cbef554
boatpand <tanapat.chnvp@gmail.com>
2022-03-21 15:58:18 +0700

    edit procfile

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 import requests
+import os
+
+port = int(os.environ.get('PORT', 5000))
 
 app = Flask(__name__)
 
@@ -143,4 +146,4 @@ def index():
     return gold
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
